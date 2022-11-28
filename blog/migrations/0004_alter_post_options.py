2c25d59bd3b25ecd8fd705a9335a771afb399515
Mitchell Burr <burrmit@gmail.com>
2022-11-28 11:53:22 -0500

    updating migrations

--- blog/migrations/0004_alter_post_options.py
+++ blog/migrations/0004_alter_post_options.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0003_delete_weirdstuff'),
+        ('blog', '0001_initial'),
     ]
 
     operations = [
--- blog/migrations/0018_contact.py
+++ blog/migrations/0018_contact.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0017_delete_comment'),
+        ('blog', '0013_alter_post_topics'),
     ]
 
     operations = [
